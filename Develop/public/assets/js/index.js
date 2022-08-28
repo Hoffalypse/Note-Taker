6accf1ff350c01b4077b9eb06dc9252efb68b84c
Bryan Wienhoff <106857564+Hoffalypse@users.noreply.github.com>
2022-08-28 01:26:54 -0600

    get and post working correctly

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -26,24 +26,28 @@ const hide = (elem) => {
 let activeNote = {};
 
 const getNotes = () =>
+
   fetch('/api/notes', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
+    
   });
 
 const saveNote = (note) =>
+
   fetch('/api/notes', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
+    
   });
 
 const deleteNote = (id) =>
-  fetch(`/api/notes/${id}`, {
+  fetch(`/notes/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
---
+++ Develop/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const db = require('./db/db.json');
+const fs = require ('fs');
+const path = require('path');
+//adds unique identifyer using NPM UUID
+const uuid = require('./id-number/uuid');
+const PORT = 3001;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//adds static HTML CSS and JS
+app.use(express.static('public'));
+
+
+app.get('/notes',(req, res) => {
+  //pulls second HTML page and renders
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+  
+})
+
+app.route('/api/notes')
+.get((req,res) => {
+  
+  console.info(`${req.method} they requested.. again`);
+  //utf-8 is default character encoding used by all browsers
+  let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.send(data);
+})
+.post((req,res) => {
+  console.info(`${req.method} Post Time, Yay more data!`);
+  const madeNote = req.body;
+ //gets unique identifier  
+  madeNote.id = uuid();
+  //turns string into object
+ let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  //add now object to data 
+  data.push(madeNote);
+  
+//stringify before sending to json file 
+  fs.writeFileSync('./db/db.json', JSON.stringify(data));
+    res.status(201).json(data);
+ 
+})
+.delete((req, res) => {
+
+})
+
+app.listen(PORT, () =>
+  console.log(`listening on my alltime favorite port ${PORT}!`)
+);
